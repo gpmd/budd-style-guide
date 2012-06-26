240c6dadf791ce7078a017fcc886e7aff8190674
Matt Bailey <matt@gpmd.co.uk>
2012-06-26 18:42:29 +0100

    New and updated styling for accordions, alerts, buttons, forms, modals and wells, plus some html markup improvements.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ jQuery.noConflict();
 
 jQuery(document).ready(function($){
 
+	// Carousel
 	$('.carousel').carousel('cycle', {
 		interval: 7500,
 		pause: "hover"
 	});
+
+	// Uniform
+	$("input:checkbox").uniform();
 
 });
